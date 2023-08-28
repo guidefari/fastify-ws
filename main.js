8956775392a2508cffe08b19fe980e0ffedd1959
Guide Fari <guide.fari@flexperto.com>
2023-08-28 20:24:08 +0200

    c0mIt mEs5ag3

--- main.js
+++ main.js
@@ -5,9 +5,17 @@ const fastify = Fastify();
 await fastify.register(websocket);
 
 fastify.get("/", { websocket: true }, function wsHandler(connection, req) {
+  // Client connect
+  console.log("Client connected");
+  // Client message
   connection.socket.on("message", (message) => {
-    // message.toString() === 'hi from client'
+    console.log(`Client message: ${message}`);
+    console.log(`Client message: ${message.toString()}`);
     connection.socket.send("hi from server");
+  });
+  // Client disconnect
+  connection.socket.on("close", () => {
+    console.log("Client disconnected");
   });
 });
 
